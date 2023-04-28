6a1b5a20e5c71a8269c054a22a7e2c380f476164
kemosaaabe <sanchellos27@mail.ru>
2023-04-28 17:20:30 +0300

    added initial files created Game class created methods

---
+++ index.js
@@ -0,0 +1,232 @@
+const rows = 13;
+const cols = 20;
+
+const rowsArray = [];
+for (let i = 0; i < rows; i++) {
+    rowsArray.push(i);
+}
+
+const colsArray = [];
+for (let i = 0; i < cols; i++) {
+    colsArray.push(i);
+}
+
+class Game {
+    init() {
+        $('.field').empty();
+
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                $('.field').append(
+                    $(
+                        `<div data-row=${j} data-col=${i} class="tile tileW" style="top: ${
+                            j * 50
+                        }px; left: ${i * 50}px"></div>`
+                    )
+                );
+            }
+        }
+
+        createRoom();
+        createPasses('col');
+        createPasses('row');
+        placeObject('swords');
+        placeObject('health');
+        placeObject('hero');
+        placeObject('enemies');
+
+        $(document).keydown((e) => {
+            const currentPosCol = $('.tileP').data('col');
+            const currentPosRow = $('.tileP').data('row');
+            let newPos;
+
+            switch (e.code) {
+                case 'KeyW':
+                    newPos = currentPosRow - 1;
+                    if (
+                        $(
+                            `.tile[data-col=${currentPosCol}][data-row=${newPos}]`
+                        ).hasClass('tileW')
+                    ) {
+                        return false;
+                    }
+
+                    if (currentPosRow === 0) {
+                        return false;
+                    }
+
+                    $('.tileP').removeClass('tileP');
+
+                    $(
+                        `.tile[data-col=${currentPosCol}][data-row=${newPos}]`
+                    ).addClass('tileP');
+
+                    break;
+                case 'KeyA':
+                    newPos = currentPosCol - 1;
+                    if (
+                        $(
+                            `.tile[data-col=${newPos}][data-row=${currentPosRow}]`
+                        ).hasClass('tileW')
+                    ) {
+                        return false;
+                    }
+
+                    if (currentPosCol === 0) {
+                        return false;
+                    }
+
+                    $('.tileP').removeClass('tileP');
+
+                    $(
+                        `.tile[data-col=${newPos}][data-row=${currentPosRow}]`
+                    ).addClass('tileP');
+
+                    break;
+                case 'KeyS':
+                    newPos = currentPosRow + 1;
+                    if (
+                        $(
+                            `.tile[data-col=${currentPosCol}][data-row=${newPos}]`
+                        ).hasClass('tileW')
+                    ) {
+                        return false;
+                    }
+
+                    if (currentPosRow === rows - 1) {
+                        return false;
+                    }
+
+                    $('.tileP').removeClass('tileP');
+
+                    $(
+                        `.tile[data-col=${currentPosCol}][data-row=${newPos}]`
+                    ).addClass('tileP');
+
+                    break;
+                case 'KeyD':
+                    newPos = currentPosCol + 1;
+                    if (
+                        $(
+                            `.tile[data-col=${newPos}][data-row=${currentPosRow}]`
+                        ).hasClass('tileW')
+                    ) {
+                        return false;
+                    }
+
+                    if (currentPosCol === cols - 1) {
+                        return false;
+                    }
+
+                    $('.tileP').removeClass('tileP');
+
+                    $(
+                        `.tile[data-col=${newPos}][data-row=${currentPosRow}]`
+                    ).addClass('tileP');
+
+                    break;
+                case 'Space':
+                    e.preventDefault();
+            }
+        });
+    }
+}
+
+const createRoom = () => {
+    for (let i = 0; i < Math.floor(Math.random() * 6) + 5; i++) {
+        const randomCol = Math.floor(Math.random() * (cols + 1));
+        const randomRow = Math.floor(Math.random() * (rows + 1));
+        const randomWidth = Math.floor(Math.random() * (5 + 1)) + 3;
+        const randomHeight = Math.floor(Math.random() * (5 + 1)) + 3;
+
+        for (let i = randomRow + 1; i < randomHeight + randomRow; i++) {
+            for (let j = randomCol + 1; j < randomWidth + randomCol; j++) {
+                $(`.tile[data-col=${j}][data-row=${i}]`).removeClass('tileW');
+            }
+        }
+    }
+};
+
+const createPasses = (type) => {
+    const randomPasses = Math.floor(Math.random() * 3) + 3;
+    const passArray = [];
+    let passArrayLength = 0;
+
+    let typeArray = [];
+    switch (type) {
+        case 'col':
+            typeArray = colsArray;
+            break;
+        case 'row':
+            typeArray = rowsArray;
+            break;
+    }
+
+    while (passArrayLength != randomPasses) {
+        const randomPass =
+            typeArray[Math.floor(Math.random() * typeArray.length)];
+
+        if (
+            passArray.includes(randomPass) ||
+            passArray.includes(randomPass + 1) ||
+            passArray.includes(randomPass - 1)
+        )
+            continue;
+
+        passArray.push(randomPass);
+        passArrayLength++;
+    }
+
+    for (let i = 0; i < passArray.length; i++) {
+        $(`.tile[data-${type}=${passArray[i]}]`).removeClass('tileW');
+    }
+};
+
+const placeObject = (type) => {
+    let emptyTiles = [];
+    let count = 0;
+    let className = '';
+
+    switch (type) {
+        case 'health':
+            emptyTiles = $('.tile').not('.tileW').not('.tileSW');
+            count = 10;
+            className = 'tileHP';
+            break;
+        case 'swords':
+            emptyTiles = $('.tile').not('.tileW').not('.tileHP');
+            count = 2;
+            className = 'tileSW';
+            break;
+        case 'hero':
+            emptyTiles = $('.tile').not('.tileW').not('.tileSW').not('.tileHP');
+            count = 1;
+            className = 'tileP';
+            break;
+        case 'enemies':
+            emptyTiles = $('.tile')
+                .not('.tileW')
+                .not('.tileHP')
+                .not('.tileSW')
+                .not('.tileP');
+            count = 10;
+            className = 'tileE';
+            break;
+    }
+
+    const emptyTilesIndexes = [];
+
+    let i = 0;
+    while (i < count) {
+        const randomIndex = Math.floor(Math.random() * emptyTiles.length);
+
+        if (emptyTilesIndexes.includes(randomIndex)) continue;
+
+        emptyTilesIndexes.push(randomIndex);
+        i++;
+    }
+
+    for (let index of emptyTilesIndexes) {
+        emptyTiles[index].classList.add(className);
+    }
+};
